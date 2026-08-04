fbe65d0d1788eb9793d30dddb4c3e4d3562368f6
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-05 02:30:44 +0500

    Building JWT service for token and session validation

--- src/main/java/com/example/ecommerce/JwtService.java
+++ src/main/java/com/example/ecommerce/JwtService.java
@@ -1,11 +1,23 @@
 package com.example.ecommerce;
 
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+import static io.jsonwebtoken.security.Keys.hmacShaKeyFor;
 
 @Service
 public class JwtService {
 
     public static final String SECRET = "mystoresecretkeymystoresecretkeymystoresecretkey";
     private static final long EXPIRATION = 1000 * 60 * 60;
+
+    private Key getSigningKey()
+    {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken ()
 }
