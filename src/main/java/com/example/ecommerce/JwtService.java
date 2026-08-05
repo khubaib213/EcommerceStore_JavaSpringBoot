835fdea1e0832a0df16bddbec54cd60201315875
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-05 16:50:05 +0500

    Implemented JWT service with token Validation and Get Claims methods

--- src/main/java/com/example/ecommerce/JwtService.java
+++ src/main/java/com/example/ecommerce/JwtService.java
@@ -1,10 +1,14 @@
 package com.example.ecommerce;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
 
 import static io.jsonwebtoken.security.Keys.hmacShaKeyFor;
 
@@ -14,10 +18,39 @@ public class JwtService {
     public static final String SECRET = "mystoresecretkeymystoresecretkeymystoresecretkey";
     private static final long EXPIRATION = 1000 * 60 * 60;
 
-    private Key getSigningKey()
-    {
+    private Key getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
     }
 
-    public String generateToken ()
+    public String generateToken(String email)
+    {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public String extractEmail(String Token)
+    {
+        return getClaims(Token).getSubject();
+    }
+
+    public boolean isTokenValid (String token)
+    {
+        try{
+            return getClaims(token).getExpiration().after(new Date());
+        }catch(Exception e)
+        {
+            return false;
+        }
+    }
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
 }
