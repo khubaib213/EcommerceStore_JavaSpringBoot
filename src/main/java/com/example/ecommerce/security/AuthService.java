0201dd76ce04c23ff6ade108a5a39592fd50e5df
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-06 13:52:56 +0500

    Added Regiter function to the Auth Service @Service

--- src/main/java/com/example/ecommerce/security/AuthService.java
+++ src/main/java/com/example/ecommerce/security/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce.security;
 
 
+import com.example.ecommerce.DTOs.RegisterDTO;
 import com.example.ecommerce.entity.AuthUser;
 import com.example.ecommerce.repository.AuthUserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,16 +13,27 @@ public class AuthService {
 
     private final AuthUserRepository authUserRepository;
     private final JwtService jwtService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public AuthService(AuthUserRepository authUserRepository, JwtService jwtService, BCryptPasswordEncoder bCryptPasswordEncoder)
+    public AuthService(AuthUserRepository authUserRepository, JwtService jwtService)
     {
         this.authUserRepository=authUserRepository;
         this.jwtService=jwtService;
-        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+        this.[passwordEncoder=new BCryptPasswordEncoder();
     }
 
-    public String Register()
+    public String Register(RegisterDTO request)
+    {
+        if(authUserRepository.findByEmail(request.getEmail()).isPresent())
+        {
+            throw new RuntimeException("Email already Exists");
+        }
+        String hashedPassword =passwordEncoder.encode(request.getPassword());
+        AuthUser user = new AuthUser(request.getEmail(), hashedPassword, "Role_User");
+        authUserRepository.save(user);
+        return jwtService.generateToken(request.getEmail());
+    }
+
 
 
 }
