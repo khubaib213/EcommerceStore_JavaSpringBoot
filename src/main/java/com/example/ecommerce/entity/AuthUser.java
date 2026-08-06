0a85a624c30bb01d340554f8586680628402dca1
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-06 12:12:10 +0500

    Created auth_user entity with Unique Id and user credentials

--- src/main/java/com/example/ecommerce/entity/AuthUser.java
+++ src/main/java/com/example/ecommerce/entity/AuthUser.java
@@ -4,4 +4,50 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "auth_users")
 public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+
+    public AuthUser(){};
+    public AuthUser(String email, String password, String role)
+    {
+        this.email=email;
+        this.password=password;
+        this.role=role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
 }
