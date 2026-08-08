cd3b2b754a35d786652c644b6aec9f44f356af97
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-08 15:48:57 +0500

    Craeted Product Entity and a Repository with custom query to search products by keywords

---
+++ src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByCategoryId (int categoryId);
+
+    @Query("SELECT p FROM PRODUCT p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> searchProducts(@Param("keyword") String keyword);
+}
