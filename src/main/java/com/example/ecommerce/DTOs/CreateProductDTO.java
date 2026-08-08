e42278662b38aabc2b8bc4863aae739da8e577c7
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-08 16:20:45 +0500

    Created CreateProductDTO.java with validation check

---
+++ src/main/java/com/example/ecommerce/DTOs/CreateProductDTO.java
@@ -0,0 +1,74 @@
+package com.example.ecommerce.DTOs;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class CreateProductDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    private String description;
+
+    @Positive(message = "Price must be positive")
+    private double price;
+
+    @Min(value = 0, message = "Stock cannot be negative")
+    private int stock;
+
+    @NotNull(message = "Category is required")
+    private Integer categoryID;
+
+    public CreateProductDTO (){};
+
+    public CreateProductDTO(String name, String description, double price, int stock, Integer categoryID)
+    {
+        this.name=name;
+        this.description=description;
+        this.stock=stock;
+        this.price=price;
+        this.categoryID=categoryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Integer getCategoryID() {
+        return categoryID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+}
