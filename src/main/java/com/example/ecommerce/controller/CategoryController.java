cbbebf8b2a4aa33ec1238cb7325c435d33473c8b
Khubaib Amjad <khubaib.amjad@jeeny.me>
2026-08-10 16:12:15 +0500

    Added CategoryController.java with controller mappings

--- src/main/java/com/example/ecommerce/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce/controller/CategoryController.java
@@ -6,10 +6,10 @@ import com.example.ecommerce.DTOs.CreateCategoryDTO;
 import com.example.ecommerce.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -27,4 +27,30 @@ public class CategoryController {
     {
         return ResponseEntity.ok(categoryService.create(request));
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CategoryDTO>>findAll()
+    {
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO>getById(@PathVariable int id)
+    {
+        return ResponseEntity.ok(categoryService.getByID(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO>update(@PathVariable int id, @Valid @RequestBody CreateCategoryDTO request)
+    {
+        return ResponseEntity.ok(categoryService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String>delete(@PathVariable int id)
+    {
+        categoryService.delete(id);
+        return ResponseEntity.ok("Category Deleted Successfully");
+    }
+
 }
